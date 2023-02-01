1ea55e8b70dba22fa0d67f10f1e07f4b8254e07b
divya <divyagiddu123@gmail.com>
2023-02-01 16:35:08 +0530

    ok

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Task from './Task/Task'
+// import RegForm from './Task/Validate'
+const App = () => {
+  return (
+    <div>
+    <Task/>
+    </div>
+  )
+}
+
+export default App
